be2f45353a9a645c8cff3245ece1404891f17284
Jessica Sánchez <michelle.sr16@gmail.com>
2019-10-03 23:06:59 -0500

    created new express api with no views

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+mongoose.connect('mongodb://127.0.0.1:27017/ednaDB');
+const app = express();
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.use((error, req, res, next) => {
+	if(error) {
+		res.status(500).json({
+			message: error.message,
+			type: error.name
+		})
+	}
+})
+
+module.exports = app;
---
+++ models/user.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const SALT_ROUNDS = 10;
+
+// Creates model for Route
+const UserSchema = new mongoose.Schema({
+	name: {
+		type: String
+	},
+	username: {
+		type: String,
+		lowercase: true,
+		required: [true, 'is required'],
+		match: [/^[a-zA-Z0-9]+$/, 'is invalid'],
+		index: { unique: true }
+	},
+	email: {
+		type: String,
+		required: [true, 'is required'],
+		match: [/\S+@+\S.\S+/, 'is invalid'],
+		index: { unique: true }
+	},
+	password: {
+		type: String,
+		required: [true, 'is required']
+	}
+})
+
+UserSchema.pre('save', function(next) {
+	let user = this;
+	if(!user.isModified('password')) return next();
+
+	bcrypt.hash(user.password, SALT_ROUNDS, (err, hash) => {
+		if(err) return next(err);
+
+		user.password = hash;
+		next()
+	})
+})
+
+UserSchema.methods.comparePass = function (testPass, callback) {
+	let user = this;
+
+	bcrypt.compare(testPass, user.password, function(err, isMatch) {
+		if(err) return callback(err);
+
+		callback(null, isMatch);
+	})
+}
+
+module.exports = mongoose.model('users', UserSchema);
